6a789d3c10372945c7e6c0e95daf9ea5a409cf6c
rmartinezapp <robert.martinez.app@gmail.com>
2021-05-18 03:21:56 -0400

    fix filter sotock items

--- app/controllers/spree/admin/stock_items_controller.rb
+++ app/controllers/spree/admin/stock_items_controller.rb
@@ -6,8 +6,9 @@ module Spree
       before_action :determine_storage_location, only: :update
       before_action :variant_storage_location, only: :index
       before_action :producer_names, only: :index
-      #before_action :stock_location
-      #before_action :set_stock_location_cookie
+      before_action :stock_location
+      before_action :set_stock_location_cookie, only: %i[index]
+      before_action :set_stock_locations, only: %i[index]
 
       def index
         respond_to do |format|
@@ -27,8 +28,6 @@ module Spree
         stock_movement = stock_location.stock_movements.build(stock_movement_params)
         stock_movement.stock_item = stock_location.set_up_stock_item(variant)
 
-#        stock_movement = @stock_location.stock_movements.build(stock_movement_params)
-#        stock_movement.stock_item = @stock_location.set_up_stock_item(variant)
         if stock_movement.save
           flash[:success] = flash_message_for(stock_movement, :successfully_created)
           respond_to do |format|
@@ -71,36 +70,41 @@ module Spree
         def stock_location
           @stock_location_class ||= StockLocation.accessible_by(current_ability, :read)
           @stock_location ||=
-            if params[:action] == 'index' && params[:q].blank? || params[:q].present? && params[:q][:stock_location_id_eq].blank?
+            if cookies[:stock_location].present? && !cookies[:stock_location].to_s.eql?('0') &&
+              params[:q].blank?
+              @stock_location_class.find_by(id: cookies[:stock_location].to_s)
+            elsif params[:action] == 'index' && params[:q].blank? ||
+              cookies[:stock_location].to_s.eql?('0') && params[:stock_location_id].blank? ||
+              params[:q].present? && params[:q][:stock_location_id_eq].blank? ||
+              params[:q].present? && params[:q][:stock_location_id_eq].to_s.eql?('0') ||
               nil
             else
               @stock_location_class.find_by(id: params[:stock_location_id]) ||
               @stock_location_class.find_by(id: params[:q][:stock_location_id_eq]) ||
               @stock_location_class.find_by(name: params[:stock_location]) ||
+              spree_current_user.stock_locations.first ||
               @stock_location_class.first
             end
-
-          # @stock_location ||=
-
-          # if params[:stock_location].blank? && params[:stock_location_id].blank? &&
-          #   params[:clear_select_st].blank? && cookies[:stock_location].present?
-          #   @stock_location_class.find_by(id: cookies[:stock_location])
-
-          # elsif params[:stock_location].present? || params[:stock_location_id].present?
-          #   @stock_location_class.find_by(name: params[:stock_location]) ||
-          #   @stock_location_class.find_by(id: params[:stock_location_id])
-
-          # elsif params[:clear_select_st].present? ||
-          #   spree_current_user.stock_locations.count > 1
-          #   nil
-          # else
-          #   spree_current_user.stock_locations.first ||
-          #   @stock_location_class.first
-          # end
         end
 
         def set_stock_location_cookie
-          cookies[:stock_location] = { :value => @stock_location&.id, :expires => 12.hours.from_now }
+          value =
+            if params[:q].present? && params[:q][:stock_location_id_eq].to_s.eql?('0')
+              params[:q][:stock_location_id_eq].to_i
+            elsif params[:q].blank?
+              cookies[:stock_location]
+            else
+              @stock_location&.id
+            end
+
+          cookies[:stock_location] = { :value => value, :expires => 24.hours.from_now }
+        end
+
+        def set_stock_locations
+          @stock_locations =
+            Spree::StockLocation.all.map { |stock_location| [stock_location.name, stock_location.id] }
+
+          @stock_locations << [Spree.t('all'), 0]
         end
 
         def variant
@@ -108,19 +112,14 @@ module Spree
         end
 
         def collection
-          return @collection if @collection.present?
+          #return @collection if @collection.present?
           # params[:q] can be blank upon pagination
           params[:q] = {} if params[:q].blank?
 
-          # @collection = stock_location.
-          #   stock_items.
-          #   accessible_by(current_ability, :read).
-          #   includes({ variant: [:product, :images, option_values: :option_type] }).
-          #   order("#{ Spree::Variant.table_name }.product_id")
-
           @collection =
-            if stock_location.blank?
-              StockLocation.accessible_by(current_ability, :read).first.stock_items.
+            if stock_location.blank? && params[:q][:stock_location_id_eq].to_s.eql?('0') ||
+              stock_location.blank?
+              Spree::StockItem.all.
               accessible_by(current_ability, :read).
               includes({ variant: [:product, :images, option_values: :option_type] }).
               order("#{ Spree::Variant.table_name }.product_id")
@@ -131,10 +130,15 @@ module Spree
               order("#{ Spree::Variant.table_name }.product_id")
             end
 
+           select_all = params[:q][:stock_location_id_eq].to_s.eql?('0')
+           params[:q][:stock_location_id_eq] = '' if select_all
+
           @search = @collection.ransack(params[:q])
           @collection = @search.result.
             page(params[:page]).
             per(params[:per_page] || SpreeOnePageStockManagement::Config[:stock_items_per_page])
+          params[:q][:stock_location_id_eq] = '0' if select_all
+          @collection
         end
 
         def variant_storage_location
